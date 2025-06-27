28bb819eee5686ccd1fde95180d727d97bf53d7f
Gulla Maneesh <gullamanish2045@gmail.com>
2025-06-27 12:05:35 +0530

    weather app

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,11 +1,12 @@
 export const getWeather = async (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=hyderbad&appid=${
     import.meta.env.VITE_API_KEY
   }&units=metric`;
 
   try {
     const res = await fetch(url);
     const data = await res.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error("Error fetching weather:", error);
